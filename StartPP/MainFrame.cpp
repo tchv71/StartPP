7976e5969f2d4608396aae5b942537f95292e350
tchv71 <tchv71@mail.ru>
2016-06-27 19:26:16 +0300

    Minor fix

--- StartPP/MainFrame.cpp
+++ StartPP/MainFrame.cpp
@@ -4,7 +4,7 @@
 #include "StartPPView.h"
 
 MainFrame::MainFrame(wxDocManager *manager, wxWindow* parent)
-    : MainFrameBaseClass(manager, (wxFrame*)parent)
+    : MainFrameBaseClass(manager, static_cast<wxFrame*>(parent))
 {
     //m_mgr->AddPane(m_auibarFilter, wxAuiPaneInfo().Caption(wxT("Фильтры")).Direction(wxAUI_DOCK_TOP).Layer(0).Row(0).Position(0).Fixed().CaptionVisible(true).MaximizeButton(false).CloseButton(true).MinimizeButton(false).PinButton(false).ToolbarPane());
     //m_mgr->AddPane(m_auibarView, wxAuiPaneInfo().Caption(wxT("Навигация")).Direction(wxAUI_DOCK_TOP).Layer(0).Row(0).Position(0).CaptionVisible(true).MaximizeButton(false).CloseButton(false).MinimizeButton(false).PinButton(false).ToolbarPane());
