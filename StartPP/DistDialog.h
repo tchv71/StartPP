b82ca20211a167b53acbdf53d6ebc4bee2e39810
tchv71 <tchv71@gmail.com>
2016-08-28 10:54:00 +0300

    Fix comments

--- StartPP/DistDialog.h
+++ StartPP/DistDialog.h
@@ -2,20 +2,20 @@
 #include "PipePresenter.h"
 
 
-// ?????????? ???? CDistDialog
+// диалоговое окно CDistDialog
 
 class CDistDialog : public CDialog
 {
 	//DECLARE_DYNAMIC(CDistDialog)
 
 public:
-	CDistDialog(CPipePresenter& pipes, CWnd* pParent = nullptr) : CDialog(pParent, wxID_ANY, _T("")), m_pipes(pipes) {}; // ??????????? ???????????
+	CDistDialog(CPipePresenter& pipes, CWnd* pParent = nullptr) : CDialog(pParent, wxID_ANY, _T("")), m_pipes(pipes) {}; // стандартный конструктор
 	virtual ~CDistDialog() {};
 
 
 protected:
 	CPipePresenter& m_pipes;
-	void DoDataExchange(CDataExchange* pDX); // ????????? DDX/DDV
+	void DoDataExchange(CDataExchange* pDX); 
 
 	//DECLARE_MESSAGE_MAP()
 public:
