ebfea0ff5bcead0103f2cd0df8a1f58aceb5257b
tchv7 <tchv7@DESKTOP-S9GAHR5>
2017-01-04 14:59:58 +0300

    Fix pipe desc dialog

--- StartPP/Resource.h
+++ StartPP/Resource.h
@@ -1,6 +1,6 @@
 //{{NO_DEPENDENCIES}}
-// Microsoft Visual C++ generated include file.
-// Used by StartPP.rc
+// Включаемый файл, созданный в Microsoft Visual C++.
+// Используется StartPP.rc
 //
 #define IDD_ABOUTBOX                    100
 #define IDP_OLE_INIT_FAILED             100
@@ -508,7 +508,7 @@
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        414
+#define _APS_NEXT_RESOURCE_VALUE        415
 #define _APS_NEXT_COMMAND_VALUE         32895
 #define _APS_NEXT_CONTROL_VALUE         1038
 #define _APS_NEXT_SYMED_VALUE           315
