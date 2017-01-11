bd2b6ea3688f30640115b1b859515510adc3f7ea
tchv71 <tchv71@mail.ru>
2017-01-11 19:31:35 +0300

    1

--- StartPP/DibGlSurface.cpp
+++ StartPP/DibGlSurface.cpp
@@ -208,6 +208,11 @@ void CDibGlSurface::DoDraw(wxDC* pDC, wxRect rectPrint)
         wxGCDC gc(*dynamic_cast<wxPrinterDC*>(pDC));
         gc.GetGraphicsContext()->DrawBitmap(bmp,rectPrint.x, rectPrint.y, rectPrint.width, rectPrint.height);
     }
+	else if (dynamic_cast<wxEnhMetaFileDC*>(pDC))
+	{
+		wxGCDC gc(*dynamic_cast<wxEnhMetaFileDC*>(pDC));
+		gc.GetGraphicsContext()->DrawBitmap(bmp, rectPrint.x, rectPrint.y, rectPrint.width, rectPrint.height);
+	}
 #endif
 }
 #endif
