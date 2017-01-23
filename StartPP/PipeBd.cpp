68becc5ba440285915ef2800750a0421955a4c50
tchv71 <tchv71@mail.ru>
2017-01-23 12:38:25 +0300

    Revert "Some refactoring"
    This reverts commit 8fcee00721bf6e120822c177a27de5f62cd82484.

---
+++ StartPP/PipeBd.cpp
@@ -0,0 +1,21 @@
+// PipeBd.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "PipeBd.h"
+
+
+// CPipeBd
+
+CPipeBd::CPipeBd()
+{
+}
+
+CPipeBd::~CPipeBd()
+{
+}
+
+
+// CPipeBd member functions
+
+
---
+++ StartPP/PipeBd.h
@@ -0,0 +1,11 @@
+#pragma once
+
+// CPipeBd command target
+
+class CPipeBd : public CDatabase
+{
+public:
+	CPipeBd();
+	virtual ~CPipeBd();
+};
+
